8af068c26e5c1f2bbffa8e8db6d0349b107614b5
George Fonseca <george.fons@gmail.com>
2023-03-15 02:43:43 -0300

    first commit

---
+++ handleSubmitForm.js
@@ -0,0 +1,8 @@
+function handleSubmitForm() {
+  const emailElement = document.getElementById('emailField');
+  const passwordElement = document.getElementById('passwordField');
+
+  console.log("Email:", emailElement.value)
+  console.log("Senha:", passwordElement.value)
+  console.log("Dados enviados com sucesso:")
+}
